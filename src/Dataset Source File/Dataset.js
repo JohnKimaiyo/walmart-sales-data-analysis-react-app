197b2deceef6dfb0dcdef2b315c24ff9125aec42
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-20 18:52:39 +0300

    second commit

---
+++ src/Dataset Source File/Dataset.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Dataset() {
+  return (
+    <div >
+      <a
+        className="App-link"
+        href="https://www.kaggle.com/datasets/polartech/walmart-grocery-product-dataset"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Click here to downlod Dataset
+      </a>
+    </div>
+  );
+}
