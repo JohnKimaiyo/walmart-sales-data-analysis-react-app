1fa527b93081da14c71e6438065e5107ab4dbcd2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-21 13:43:47 +0300

    image upload

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -16,27 +16,47 @@ export default function Header() {
           <td>Sales Manager</td>
           <td>To get a dashboard overview of Sales</td>
           <td>Can follow better which customers and products sells the best</td>
-          <td>A Power BI dashboard which updates data once a day</td>
+          <td></td>
         </tr>
         <tr>
           <td>Sales Representative</td>
-          <td>A detailed overview of Internet Sales per Customers</td>
+          <td>A detailed overview of Sales per Product</td>
           <td>
             Can follow up my customers that buys the most and who we can sell
             ore to
           </td>
           <td>
             A Power BI dashboard which allows me to filter data for each
-            customer
+            Product
           </td>
         </tr>
         <tr>
           <td>Sales Representative</td>
-          <td>A detailed overview of Internet Sales per Products</td>
-          <td>Can follow up my Products that sells the most</td>
+          <td>A detailed overview of Sales per Brand</td>
+          <td>
+            Can follow up my customers that buys the most and who we can sell
+            ore to
+          </td>
+          <td>
+            A Power BI dashboard which allows me to filter data for each
+            Brand
+          </td>
+        </tr>
+        <tr>
+          <td>Sales Representative</td>
+          <td>A detailed overview of  Sales per  Department</td>
+          <td>Can follow up my Department that sells the most</td>
 
           <td>
-            A Power BI dashboard which allows me to filter data for each Product
+            A Power BI dashboard which allows me to filter data for each Department
+          </td>
+        </tr><tr>
+          <td>Sales Representative</td>
+          <td>A detailed overview of  Sales per  Category</td>
+          <td>Can follow up my Department that sells the most</td>
+
+          <td>
+            A Power BI dashboard which allows me to filter data for each Category
           </td>
         </tr>
       </table>
@@ -55,7 +75,21 @@ export default function Header() {
 
           <td>
             <a
-              href="https://github.com/JohnKimaiyo/walmart-sales-data-analysis-react-app/tree/main/src/Visual%20Report"
+              href="https://github.com/JohnKimaiyo/walmart-sales-data-analysis-react-app/blob/main/src/Visual%20Report/Walamart%20Sales%20Analysis.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View the Dashboard Here
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>Visualised Data</td>
+          <td>PDF file containing the Sales Visualised Data</td>
+
+          <td>
+            <a
+              href="https://github.com/JohnKimaiyo/walmart-sales-data-analysis-react-app/blob/main/src/Visual%20Report/Walamart%20Sales%20Analysis.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
