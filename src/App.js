767034ac93f75fd57e2a71811526fe249bc52c6d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-21 20:18:44 +0300

    adding json files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 
+
 import Header from "./Header/Header";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
-     
+    
     </div>
   );
 }
