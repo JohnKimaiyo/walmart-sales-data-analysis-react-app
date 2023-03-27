01548cb52a031a385869eb8dfc4c3b0a547c0a67
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-27 20:13:01 +0300

    final commt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Header from "./Header/Header";
-
+import Layout from "./Pages/Layout";
+import BusinessRequest from "./Pages/BusinessRequest";
+import PowerBIDashboard from "./Pages/PowerBIDashboard";
+import GoogleSheet from "./Pages/GoogleSheetDashbaord";
+import ExcelSheetDashboard from "./Pages/ExcelSheetDashboard";
+import SQL from "./Pages/SQL";
+import JSON from "./Pages/JSON";
+import SourceFiles from "./Pages/SourceFiles";
+import NoPage from "./Pages/NoPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
+          <Route index element={<BusinessRequest />} />
+          <Route path="PowerBIDashboard" element={<PowerBIDashboard />} />
+          <Route path="GoogleSheet" element={<GoogleSheet />} />
+          <Route path="ExcelSheetDashboard" element={<ExcelSheetDashboard />} />
+
+          <Route path="SQL" element={<SQL />} />
+
+          <Route path="JSON" element={<JSON />} />
+          <Route path="SourceFiles" element={<SourceFiles />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -9,10 +9,25 @@ const Layout = () => {
             <Link to="/">BusinessRequest</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/PowerBIDashboard">PowerBIDashboard</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/GoogleSheetDashboard">GoogleSheetDashboard</Link>
+          </li>
+          <li>
+            <Link to="/ExcelSheetDashboard">ExcelSheetDashboard</Link>
+          </li>
+          <li>
+            <Link to="/SQL">SQL</Link>
+          </li>
+          <li>
+            <Link to="/SourceFiles">SourceFiles</Link>
+          </li>
+          <li>
+            <Link to="/JSON">JSON</Link>
+          </li>
+          <li>
+            <Link to="/NoPage">NoPage</Link>
           </li>
         </ul>
       </nav>
