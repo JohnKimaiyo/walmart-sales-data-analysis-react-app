d45fd590576a1e8167194ef37b703e3fa2e31045
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-28 17:50:45 +0300

    final commit

---
+++ Backend/SQL/querry.sql
@@ -0,0 +1,5 @@
+
+
+/******  SELECT MAX([Fuel_Price]) ******/
+SELECT  * FROM [walmart sales].[dbo].[Walmart sales data]
+  SELECT MAX([Fuel_Price]) AS Highest_Cost  FROM [walmart sales].[dbo].[Walmart sales data];
